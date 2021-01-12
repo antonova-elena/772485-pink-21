41457b485c877239230562b5c2c3ffe8394cecda
Elena Antonova <antonova.elena.khv@gmail.com>
2021-01-12 22:33:24 +0300

    Исправляет js

--- source/js/main.js
+++ source/js/main.js
@@ -2,13 +2,14 @@
   const navToggleElement = navElement.querySelector('.nav__toggle');
 
   navElement.classList.remove('nav--nojs');
+  navElement.classList.add('nav--closed')
 
-  navToggleElement.addEventListener('click', function() {
-    if (navElement.classList.contains('nav--closed')) {
-      navElement.classList.remove('nav--closed');
-      navElement.classList.add('nav--opened');
-    } else {
-      navElement.classList.add('nav--closed');
+  navToggleElement.addEventListener('click', () => {
+    if (navElement.classList.contains('nav--opened')) {
       navElement.classList.remove('nav--opened');
+      navElement.classList.add('nav--closed');
+    } else {
+      navElement.classList.add('nav--opened');
+      navElement.classList.remove('nav--closed');
     }
   });
